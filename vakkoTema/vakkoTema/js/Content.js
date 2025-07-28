ca78cd4254b554c2f5e0da22297c959692d9ec8c
Abdulkadir Doğan <117775638+akadirdgn@users.noreply.github.com>
2025-07-28 17:20:19 +0300

    Delete vakkoTema directory

--- vakkoTema/vakkoTema/js/Content.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.more-detail-button').forEach(button => {
-  button.addEventListener('click', function() {
-    const content = this.closest('section').querySelector('.about-content-inner, .description-text-container');
-    content.classList.toggle('expanded');
-    this.textContent = content.classList.contains('expanded') ? 'Less detail' : 'More detail';
-  });
-});
